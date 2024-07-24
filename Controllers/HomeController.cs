fe0498663b60bacfa35d300e080c644272c20844
Cavid <Cavid@DESKTOP-E791T53>
2024-07-24 16:56:07 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Entities;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(GetAllProducts());
+        }
+
+        public IActionResult Drinks()
+        {
+
+            return View(GetDrinks());
+        }
+
+        public IActionResult HotMeals()
+        {
+            return View(GetHotMeals());
+        }
+
+        public IActionResult FastFoods()
+        {
+            return View(GetFastFoods());
+        }
+
+        public List<Drink> GetDrinks()
+        {
+            List<Drink> drinks = new List<Drink>
+            {
+                new Drink
+                {
+                    Id = 1,
+                    Name = "Sirab",
+                },
+
+                new Drink
+                {
+                    Id= 2,
+                    Name = "Coca-Cola"
+                },
+
+                new Drink
+                {
+                    Id = 3,
+                    Name = "Bizon"
+                }
+            };
+
+            return drinks;
+        }
+
+        public List<HotMeal> GetHotMeals()
+        {
+            List<HotMeal> hotMeals = new List<HotMeal>
+            {
+                new HotMeal
+                {
+                    Id = 1,
+                    Name = "Buglama",
+                },
+
+                new HotMeal
+                {
+                    Id= 2,
+                    Name = "Jiz-Biz"
+                },
+
+                new HotMeal
+                {
+                    Id = 3,
+                    Name = "Kufte Bozbash"
+                }
+            };
+
+            return hotMeals;
+        }
+
+        public List<FastFood> GetFastFoods()
+        {
+            List<FastFood> fastFoods = new List<FastFood>
+            {
+                new FastFood
+                {
+                    Id = 1,
+                    Name = "Cheeseburger",
+                },
+
+                new FastFood
+                {
+                    Id= 2,
+                    Name = "Chicken Sandwich"
+                },
+
+                new FastFood
+                {
+                    Id = 3,
+                    Name = "Fries"
+                }
+            };
+
+            return fastFoods;
+        }
+    
+        public AllProducts GetAllProducts()
+        {
+            var all = new AllProducts(GetDrinks(), GetFastFoods(), GetHotMeals());
+            return all;
+        }
+    }
+}
---
+++ Models/AllProducts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Models
+{
+    public class AllProducts
+    {
+        public List<Drink> Drinks { get; set; }
+        public List<FastFood> FastFoods { get; set; }
+        public List<HotMeal> HotMeals { get; set; }
+
+        public AllProducts()
+        {
+
+        }
+
+        public AllProducts(List<Drink> drinks, List<FastFood> fastFoods, List<HotMeal> hotMeals)
+        {
+            Drinks = drinks;
+            FastFoods = fastFoods;
+            HotMeals = hotMeals;
+        }
+    }
+}
